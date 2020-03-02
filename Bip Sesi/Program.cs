8246c4b4f3054db77206f2bfa49210b4e2eb26c8
resatakkus <48046647+resatakkus@users.noreply.github.com>
2020-03-02 22:17:41 +0300

    Project upload

---
+++ Bip Sesi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bip_Sesi
+{//Kullanıcıya Kaç yaşında olduğunu sorun
+    //En güzel yaşın kullanıcının yaşı olduğunu belirtin
+    //Bip sesiyle programı sonlandırın
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç yaşındasınız");
+            var yaş = Console.ReadLine();
+            Console.WriteLine("En güzel yaş: " + yaş);
+            Console.ReadLine();
+            Console.Beep();
+
+        }
+    }
+}
